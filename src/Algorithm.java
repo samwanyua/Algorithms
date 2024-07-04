6dd7ddea59a09b06acae27525310c6e0a2a715ee
samwanyua <samexample8@gmail.com>
2024-07-04 19:09:20 +0300

    Removing even numbers from a given array

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,8 +1,13 @@
 import Array.Array;
 
+import java.util.Arrays;
+
 public class Algorithm {
     public static void main(String[] args) {
         Array array = new Array();
         array.arrayDemo();
+        int[] arr = {3,2,4,7,10,6,5};
+        int[] result = array.removeEvenNumbers(arr);
+        System.out.println(Arrays.toString(result));
     }
 }
--- src/Array/Array.java
+++ src/Array/Array.java
@@ -27,5 +27,28 @@ public class Array {
         // accessing the last element
         System.out.println(age.length - 1);
     }
+    // removing elements in an integer
+    // ex. remove even numbers in a given array
+    // ex. input arr = {3,2,4,7,10,6,5}
+    //      output arr = {3,7,5}
+    int[] arr = {3,2,4,7,10,6,5};
+    public int[] removeEvenNumbers(int[] arr){
+        int oddCount = 0;
+        for (int i : arr) {
+            if (i % 2 != 0) {
+                oddCount++;
+            }
+        }
+
+        int[] result = new int[oddCount];
+        int index = 0;
+        for (int j : arr) {
+            if (j % 2 != 0) {
+                result[index] = j;
+                index++;
+            }
+        }
+        return result;
+    }
 
 }
