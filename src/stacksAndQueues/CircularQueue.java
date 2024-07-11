0fb532b75a402c3fd17c27952e0fac96f3da53f6
samwanyua <samexample8@gmail.com>
2024-07-11 18:07:00 +0300

    Implement insert method in a circular queue

---
+++ src/stacksAndQueues/CircularQueue.java
@@ -0,0 +1,37 @@
+package stacksAndQueues;
+
+public class CircularQueue {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    protected int end = 0;
+    protected int front = 0;
+    protected int size = 0;
+
+    public CircularQueue() {
+        this(DEFAULT_SIZE);
+    }
+
+    public CircularQueue(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean isFull(){
+        return size == data.length; // pointer at the last index
+    }
+
+    public boolean isEmpty(){
+        return end == 0;
+    }
+
+    public boolean insert(int item){
+        if(isFull()){
+            return false;
+        }
+        data[end] = item;
+        end++; // same as data[end++] = item;
+        end = end % data.length;
+        size++;
+        return true;
+    }
+}
