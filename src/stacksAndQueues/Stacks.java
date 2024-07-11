d166124f2da770d1ea2deabb429902c74e45a41a
samwanyua <samexample8@gmail.com>
2024-07-11 14:33:24 +0300

    Introduction to stacks

--- src/stacksAndQueues/Stacks.java
+++ src/stacksAndQueues/Stacks.java
@@ -8,9 +8,14 @@ public class Stacks {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         stack.push(34); // push is to insert
-        stack.push(87);
+        stack.push(87); // O(n)
+        stack.push(234);
+        stack.push(324);
+        stack.push(45);
 
-        System.out.println(stack.pop());
+        System.out.println(stack.pop()); // to remove O(n)
+        System.out.println(stack.size());
+        System.out.println(stack); // to print a stack
 
 
     }
