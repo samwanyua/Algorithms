5e86282d993dd1d4e7ad9e4ecd7e19623a03512d
samwanyua <samexample8@gmail.com>
2024-07-11 14:57:48 +0300

    Introduction to queues

--- src/stacksAndQueues/Queues.java
+++ src/stacksAndQueues/Queues.java
@@ -1,5 +1,7 @@
 package stacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -14,6 +16,16 @@ public class Queues {
 
         System.out.println(queue.peek()); // peeks the head
         System.out.println(queue.remove()); // removes the head
+        System.out.println(queue.offer(4));
 
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(89);
+        deque.addFirst(45);
+        deque.addLast(32);
+
+        System.out.println(deque);
+        System.out.println(deque.removeLast());
+        System.out.println(deque.offerFirst(4)); // inserts at the front of the deque unless it would violate capacity restrictions
+        System.out.println(deque);
     }
 }
