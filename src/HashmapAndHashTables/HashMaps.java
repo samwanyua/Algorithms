92bd45fdf671c26f085bd3b10bc1f1edba960595
samwanyua <samexample8@gmail.com>
2024-07-12 13:48:00 +0300

    Introduction to HashMaps

--- src/HashmapAndHashTables/HashMaps.java
+++ src/HashmapAndHashTables/HashMaps.java
@@ -1,4 +1,9 @@
 package HashmapAndHashTables;
 
 public class HashMaps {
+    public static void main(String []args){
+        String comment = "Better";
+        int count = comment.hashCode();
+        System.out.println(count);
+    }
 }
