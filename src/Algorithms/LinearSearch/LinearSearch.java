146f1660804d3816dc31af7d3d44e84b645d17ac
samwanyua <samexample8@gmail.com>
2024-07-13 13:02:14 +0300

    Implement linear search

--- src/Algorithms/LinearSearch/LinearSearch.java
+++ src/Algorithms/LinearSearch/LinearSearch.java
@@ -1,6 +1,15 @@
 package Algorithms.LinearSearch;
 
 public class LinearSearch {
+    private static int searchLinearly(int[] array, int value) {
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == value){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] array = {9,1,84,53,55,32,45,3,34,341,32,324,34213};
         int index = searchLinearly(array, 341);
@@ -11,15 +20,6 @@ public class LinearSearch {
         else{
             System.out.println("Element not found");
         }
-    }
-
-    private static int searchLinearly(int[] array, int value) {
-        for(int i = 0; i < array.length; i++){
-            if(array[i] == value){
-                return i;
-            }
-        }
-        return -1;
     }
 
 
