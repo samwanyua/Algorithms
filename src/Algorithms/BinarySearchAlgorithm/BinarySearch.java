6f6c685c80f5e5cec2cd61f2247b3b5d326cd4c4
samwanyua <samexample8@gmail.com>
2024-07-14 21:44:19 +0300

    Introduction to Binary search

--- src/Algorithms/BinarySearchAlgorithm/BinarySearch.java
+++ src/Algorithms/BinarySearchAlgorithm/BinarySearch.java
@@ -9,6 +9,7 @@ public class BinarySearch {
 
         for(int i = 10; i < array.length; i++){
             array[i] = i;
+            System.out.println("Print"); //sample
         }
 
         int result = Arrays.binarySearch(array,target);
